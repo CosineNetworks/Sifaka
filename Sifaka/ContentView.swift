3061cfd7c2dbfe85834ad45cefc03ae33db81f4e
Aiden Fox Ivey <aidenfoxivey@disroot.org>
2023-05-10 12:48:44 -0400

    Add a mockup

--- Sifaka/ContentView.swift
+++ Sifaka/ContentView.swift
@@ -7,17 +7,25 @@
 
 import SwiftUI
 
+func m() {
+    
+}
 struct ContentView: View {
+    @State private var downloadAmount = 0.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Text("Cosine Newtorks")
+            .font(.title)
+        Text("Like the internet but easier lmfao.")
+            Button(action:{
+                print("Hello World!")
+            }) {Text("Connect")}
         }
-        .padding()
+        ProgressView("Downloading…", value: downloadAmount, total: 100)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
